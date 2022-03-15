86d284d6460b77f99aaaaa004ea77c8cc52f331e
Justin Sixsmith <justinsixsmith@gmail.com>
2022-03-15 16:05:09 -0500

    builds some simple class stuff

---
+++ src/Contact.java
@@ -0,0 +1,25 @@
+public class Contact {
+    private String name;
+    private long phoneNumber;
+
+    public Contact(String name, long phoneNumber) {
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public long getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(long phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+}
